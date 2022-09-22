d52fc6ba6656d489fb3629222cb8d4a6df47aca2
mohmmedjar <mohmmed.jouzef@gmail.com>
2022-09-22 10:28:40 +0300

    Main Task

---
+++ data2.js
@@ -0,0 +1,10 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscsd2",
+      name: "Building a mosque",
+      price: 15000,
+      desc: "",
+      img:"images/pic4.jpg",
+    },
+
+  ];
---
+++ data3.js
@@ -0,0 +1,10 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscsd3",
+      name: "6/6 Corrective Eyeglasses",
+      price: 200,
+      desc: "",
+      img: "images/pic3.jpg",
+    },
+
+  ];
